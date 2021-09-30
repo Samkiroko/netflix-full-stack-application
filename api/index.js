d6fcfbfa4f0f8b461c47c9adb767a3991593aacf
Samkiroko <kirokodev@gmail.com>
2021-09-30 12:37:03 +0300

    Adding models and routes

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const authRoute = require('./routes/auth')
+
+dotenv.config()
+
+main().catch((err) => console.log(err))
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL).then(() => console.log('db connection Successful'))
+}
+
+app.use(express.json())
+
+app.use('/api/auth', authRoute)
+
+app.listen(8800, () => {
+  console.log('Backend server is running')
+})
