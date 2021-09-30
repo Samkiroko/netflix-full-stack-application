f1eb18566137c5a58a7ad8e7e6cf6a1bf61a5b60
Samkiroko <kirokodev@gmail.com>
2021-09-30 18:42:06 +0300

    adding random movie and get all movies

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -42,64 +42,52 @@ router.put('/:id', verify, async (req, res) => {
 })
 // Delete
 router.delete('/:id', verify, async (req, res) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
+  if (req.user.isAdmin) {
     try {
-      await User.findByIdAndDelete(req.params.id)
-      res.status(200).json('User has been deleted .... ')
+      await Movie.findByIdAndDelete(req.params.id)
+      res.status(200).json('The movie has been deleted')
     } catch (err) {
       res.status(500).json(err)
     }
   } else {
-    res.status(403).json('You can delete only your account!')
+    res.status(403).json('You are not allowed!')
   }
 })
 // Get
-router.get('/find/:id', async (req, res) => {
+router.get('/find:id', verify, async (req, res) => {
   try {
-    const user = await User.findById(req.params.id)
-    const { password, ...info } = user._doc
-    res.status(200).json(info)
+    const movie = await Movie.findById(req.params.id)
+    res.status(200).json(movie)
   } catch (err) {
     res.status(500).json(err)
   }
 })
-// Get All
+// Get random
+router.get('/random', verify, async (req, res) => {
+  const type = req.query.type
+  try {
+    if (type == 'series') {
+      movie = await Movie.aggregate([{ $match: { isSeries: true } }, { $sample: { size: 1 } }])
+    } else {
+      movie = await Movie.aggregate([{ $match: { isSeries: false } }, { $sample: { size: 1 } }])
+    }
+    res.status(200).json(movie)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// Get All movies
 router.get('/', verify, async (req, res) => {
-  const query = req.query.new
   if (req.user.isAdmin) {
     try {
-      const users = query ? await User.find().sort({ _id: -1 }).limit(10) : await User.find()
-      res.status(200).json(users)
+      const movies = await Movie.find()
+      res.status(200).json(movies.reverse())
     } catch (err) {
       res.status(500).json(err)
     }
   } else {
-    res.status(403).json('You are not allowed to see all users!')
-  }
-})
-
-// Get user stats
-router.get('/stats', async (req, res) => {
-  const today = new Date()
-  const latYear = today.setFullYear(today.setFullYear() - 1)
-
-  try {
-    const data = await User.aggregate([
-      {
-        $project: {
-          month: { $month: '$createdAt' },
-        },
-      },
-      {
-        $group: {
-          _id: '$month',
-          total: { $sum: 1 },
-        },
-      },
-    ])
-    res.status(200).json(data)
-  } catch (err) {
-    res.status(500).json(err)
+    res.status(403).json('You are not allowed')
   }
 })
 
