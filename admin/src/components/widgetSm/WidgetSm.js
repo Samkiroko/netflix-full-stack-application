cad0b2f101ac876ced7cb741d8d426d916941ffc
Samkiroko <kirokodev@gmail.com>
2021-10-04 11:37:38 +0300

    Adding new users

--- admin/src/components/widgetSm/WidgetSm.js
+++ admin/src/components/widgetSm/WidgetSm.js
@@ -1,79 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './widgetSm.css'
-import avatar from '../../images/avatar2.jpg'
+import avatar from '../../images/avatar-1.jpeg'
 import { Visibility } from '@material-ui/icons'
+import axios from 'axios'
 
 const WidgetSm = () => {
+  const [newUsers, setNewUsers] = useState([])
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get('/users?new=true', {
+          headers: {
+            token:
+              ' Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNTU4Zjk4MGE2MjJlMzU4ZjdlNTNmNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzMzNjUzNywiZXhwIjoxNjMzNzY4NTM3fQ.tKRp52Z9h7t0PnMhxk6M2xdautmuTG_s1EXBwBBuGAA',
+          },
+        })
+        setNewUsers(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getNewUsers()
+  }, [])
   return (
     <div className='widgetSm'>
       <span className='widgetSmTitle'>New Join Members</span>
       <ul className='widgetSmList'>
-        <li className='widgetSmListItem'>
-          <img src={avatar} alt='avatar' className='widgetSmImg' />
-          <div className='widgetSmUser'>
-            <span className='widgetSmUsername'>Sam kiroko</span>
-            <span className='widgetSmUserTitle'>software Engineer</span>
-          </div>
-          <button className='widgetSmButton'>
-            <Visibility className='widgetSmIcon' />
-            Display
-          </button>
-        </li>
-        <li className='widgetSmListItem'>
-          <img src={avatar} alt='avatar' className='widgetSmImg' />
-          <div className='widgetSmUser'>
-            <span className='widgetSmUsername'>Sam kiroko</span>
-            <span className='widgetSmUserTitle'>software Engineer</span>
-          </div>
-          <button className='widgetSmButton'>
-            <Visibility className='widgetSmIcon' />
-            Display
-          </button>
-        </li>{' '}
-        <li className='widgetSmListItem'>
-          <img src={avatar} alt='avatar' className='widgetSmImg' />
-          <div className='widgetSmUser'>
-            <span className='widgetSmUsername'>Sam kiroko</span>
-            <span className='widgetSmUserTitle'>software Engineer</span>
-          </div>
-          <button className='widgetSmButton'>
-            <Visibility className='widgetSmIcon' />
-            Display
-          </button>
-        </li>{' '}
-        <li className='widgetSmListItem'>
-          <img src={avatar} alt='avatar' className='widgetSmImg' />
-          <div className='widgetSmUser'>
-            <span className='widgetSmUsername'>Sam kiroko</span>
-            <span className='widgetSmUserTitle'>software Engineer</span>
-          </div>
-          <button className='widgetSmButton'>
-            <Visibility className='widgetSmIcon' />
-            Display
-          </button>
-        </li>{' '}
-        <li className='widgetSmListItem'>
-          <img src={avatar} alt='avatar' className='widgetSmImg' />
-          <div className='widgetSmUser'>
-            <span className='widgetSmUsername'>Sam kiroko</span>
-            <span className='widgetSmUserTitle'>software Engineer</span>
-          </div>
-          <button className='widgetSmButton'>
-            <Visibility className='widgetSmIcon' />
-            Display
-          </button>
-        </li>{' '}
-        <li className='widgetSmListItem'>
-          <img src={avatar} alt='avatar' className='widgetSmImg' />
-          <div className='widgetSmUser'>
-            <span className='widgetSmUsername'>Sam kiroko</span>
-            <span className='widgetSmUserTitle'>software Engineer</span>
-          </div>
-          <button className='widgetSmButton'>
-            <Visibility className='widgetSmIcon' />
-            Display
-          </button>
-        </li>
+        {newUsers.map((user) => (
+          <li className='widgetSmListItem'>
+            <img src={user.profilePic || avatar} alt='avatar' className='widgetSmImg' />
+            <div className='widgetSmUser'>
+              <span className='widgetSmUsername'>{user.username}</span>
+            </div>
+            <button className='widgetSmButton'>
+              <Visibility className='widgetSmIcon' />
+              Display
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   )
